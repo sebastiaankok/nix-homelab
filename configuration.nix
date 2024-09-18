a65cf91fa62c1c538169582cd4a6d6e252556848
Sebastiaan Kok <sebastiaan@linux.com>
2024-09-18 15:42:53 +0200

    Add path

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./services
+      ./services/media_servers.nix
     ];
 
   # Allow unfree packages
