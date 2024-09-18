fdeca127227ff2af751706c1c4417e64aa76a209
Sebastiaan Kok <sebastiaan@linux.com>
2024-09-18 15:16:46 +0200

    add comment

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,7 @@
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-
+  # Add virtual IP to the network interface
   networking.interfaces.wlp2s0 = {
     ipv4.addresses = [
       {
