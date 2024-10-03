0a5f22bf96e927b5b2da44bb45b5fbb0a6c29b77
Sebastiaan Kok <sebastiaan@linux.com>
2024-10-03 22:54:29 +0200

    chore: refactor + add vm

--- flake.nix
+++ flake.nix
@@ -28,26 +28,11 @@
 
       modules = [
         sops-nix.nixosModules.sops
+	      microvm.nixosModules.host
         ./hosts/dell-i5-7300U
         ./profiles
         ./modules
-        microvm.nixosModules.host
-        {
-          microvm.vms = {
-            test = {
-              # The package set to use for the microvm. This also determines the microvm's architecture.
-              # Defaults to the host system's package set if not given.
-              pkgs = import nixpkgs {system = "x86_64-linux";};
-
-              # (Optional) A set of special arguments to be passed to the MicroVM's NixOS modules.
-              #specialArgs = {};
-
-              # The configuration for the MicroVM.
-              # Multiple definitions will be merged as expected.
-              config = import ./hosts/vm-test/default.nix;
-            };
-          };
-	      }
+        ./modules/virtual/mediaserver/default.nix
       ];
     };
   };
