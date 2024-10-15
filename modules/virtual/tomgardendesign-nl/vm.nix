a2250741d70c3e7191621816cdec551ee90968f7
Sebastiaan Kok <sebastiaan@linux.com>
2024-10-15 22:25:28 +0200

    feat: add tomgardendesign wordpress site

---
+++ modules/virtual/tomgardendesign-nl/vm.nix
@@ -0,0 +1,33 @@
+{ cfg, lib, ... }:
+
+{
+  hypervisor = cfg.vm.hypervisor;
+  vcpu = cfg.vm.cpu;
+  mem = cfg.vm.memory;
+  interfaces = [
+    {
+      id = "${cfg.vm.hostname}";
+      mac = "${cfg.vm.mac}";
+      type = "macvtap";
+      macvtap  = {
+        link = "${cfg.vm.interface}";
+        mode = "${cfg.vm.network_mode}";
+      };
+    }
+  ];
+
+  shares = [
+    {
+      source = "/nix/store";
+      mountPoint = "/nix/.ro-store";
+      tag = "ro-store";
+      proto = "virtiofs";
+    }
+    {
+      source = "${cfg.dirs.contentDir}";
+      mountPoint = "/data";
+      tag = "wp-content";
+      proto = "virtiofs";
+    }
+  ];
+}
