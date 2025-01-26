40f0092652d691d76e53d0e2e95469514aaebebd
Sebastiaan Kok <sebastiaan@linux.com>
2025-01-27 00:09:16 +0100

    chore: fix renovate config

---
+++ modules/containers/zigbee2mqtt/default.nix
@@ -0,0 +1,78 @@
+{ config, lib, pkgs, ... }:
+with lib;
+
+let
+  app = "zigbee2mqtt";
+  image = "koenkk/zigbee2mqtt:1.42.0";
+  cfg = config.hostConfig.services.${app};
+  appData = config.hostConfig.dataDir + "/${app}";
+  domainName = config.hostConfig.domainName;
+  user = "${app}";
+  group = "${app}";
+  uid = 317;
+  gid = 317;
+  port = 8080;
+in
+{
+  options.hostConfig.services.${app} = {
+    enable = mkEnableOption "${app}";
+    backup = mkOption {
+      type = lib.types.bool;
+      description = "Enable backups";
+      default = true;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Create the group first, with a specific GID
+    users.groups.${group} = {
+      gid = gid;  # Set the GID explicitly
+    };
+
+    # Create the user, assign to the group, and set a specific UID
+    users.users.${user} = {
+      isSystemUser = true;  # Ensures the user is treated as a system user
+      uid = uid;  # Explicitly set the UID
+      group = "${group}";  # Assign the user to the predefined group
+      home = "${appData}";  # Define home directory for the service
+      createHome = true;  # Automatically create the home directory
+    };
+
+    systemd.tmpfiles.rules = [
+      "d ${appData} 0750 ${user} ${group} -" #The - disables automatic cleanup, so the file wont be removed after a period
+    ];
+
+    virtualisation.oci-containers.containers.${app} = {
+      autoStart = true;
+      image = "${image}";
+      user = "${toString uid}:${toString gid}";  # Convert uid and gid to strings
+      volumes = [
+        "${appData}:/app/data:rw"
+        "/etc/localtime:/etc/localtime:ro"
+      ];
+      extraOptions = [
+        "--network=host"
+      ];
+    };
+
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      virtualHosts."zigbee2mqtt.${domainName}" = {
+        useACMEHost = "${domainName}";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString port}";
+          proxyWebsockets = true;
+        };
+      };
+    };
+
+    services.restic.backups = mkIf cfg.backup (config.lib.hostConfig.mkRestic {
+     inherit app appData;
+     paths = [ appData ];
+     #excludePath = [ "./example"];
+    });
+
+  };
+}
