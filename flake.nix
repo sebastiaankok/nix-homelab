ed5e884d1cea6a5c3ee46ec0bdc6c563ec6242ef
Sebastiaan Kok <sebastiaan@linux.com>
2025-03-31 18:55:07 +0200

    feat: add dell-i5 laptop

--- flake.nix
+++ flake.nix
@@ -12,36 +12,69 @@
     microvm.url = "github:astro/microvm.nix";
     microvm.inputs.nixpkgs.follows = "nixpkgs";
 
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+      # url = "github:nix-community/nixvim/nixos-24.11";
+
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
-  outputs = inputs@{ self, nixpkgs, unstable, sops-nix, microvm, ... }: {
+  outputs = inputs@{ self, nixpkgs, unstable, sops-nix, microvm, home-manager
+    , nixvim, ... }: {
 
-    # NixOS configuration for B660-i5-13600 (homelab)
-    nixosConfigurations.b660-i5-13600 = nixpkgs.lib.nixosSystem rec {
-      system = "x86_64-linux";
+      # NixOS configuration for B660-i5-13600 (homelab)
+      nixosConfigurations.b660-i5-13600 = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
 
-      specialArgs = {
-        pkgs-unstable = import unstable {
-          config.allowUnfree = true;
-          config.permittedInsecurePackages = [
+        specialArgs = {
+          pkgs-unstable = import unstable {
+            config.allowUnfree = true;
+            config.permittedInsecurePackages = [
               "dotnet-runtime-wrapped-6.0.36"
               "aspnetcore-runtime-6.0.36"
               "aspnetcore-runtime-wrapped-6.0.36"
               "dotnet-sdk-6.0.428"
               "dotnet-sdk-wrapped-6.0.428"
-          ];
-          inherit system;
+            ];
+            inherit system;
+          };
         };
+
+        modules = [
+          sops-nix.nixosModules.sops
+          microvm.nixosModules.host
+          ./hosts/b660-i5-13600
+          ./profiles
+          ./modules
+          ./modules/virtual/mediaserver/default.nix
+          ./modules/virtual/k3s-cloudflared
+        ];
       };
 
-      modules = [
-        sops-nix.nixosModules.sops
-        microvm.nixosModules.host
-	      ./hosts/b660-i5-13600
-        ./profiles
-        ./modules
-        ./modules/virtual/mediaserver/default.nix
-        ./modules/virtual/k3s-cloudflared
-      ];
+      # Workstation
+      nixosConfigurations.dell-i5-7300U = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+        modules = [
+          sops-nix.nixosModules.sops
+          ./hosts/dell-i5-7300U
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs; };
+              sharedModules = [ nixvim.homeManagerModules.nixvim ];
+              users = { sebastiaan = import ./home/modules/default.nix; };
+            };
+          }
+        ];
+      };
     };
-  };
 }
