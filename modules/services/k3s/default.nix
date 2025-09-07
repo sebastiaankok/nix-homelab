4ba6c336c8b6975f8a736a979a562576b392c453
Sebastiaan Kok <sebastiaan@linux.com>
2025-09-07 22:11:56 +0200

    chore: update k3s service

--- modules/services/k3s/default.nix
+++ modules/services/k3s/default.nix
@@ -1,45 +1,52 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, pkgs-unstable, ... }:
 with lib;
 
 let
   app = "k3s";
   cfg = config.hostConfig.services.${app};
   domainName = config.hostConfig.domainName;
-  user = "k3s";
-  group = "k3s";
   port = 6443;
-
 in
 {
-
   options.hostConfig.services.${app} = {
     enable = mkEnableOption "${app}";
   };
 
   config = mkIf cfg.enable {
 
-    users.users.${user} = {
-      isSystemUser = true;
-      group = "${group}";
-    };
-
-    users.groups.${group} = {};
-
     # Enable k3s service
     services.k3s = {
       enable = true;
+      #package = pkgs.k3s_1_33;
       role = "server";
       extraFlags = [
         "--disable traefik"
         "--disable local-storage"
-        "--disable-network-policy"
+        #"--disable servicelb"
+        #"--disable-network-policy"
       ];
+      autoDeployCharts = {
+        argocd = {
+          name = "argo-cd";
+          targetNamespace = "argocd";
+          createNamespace = true;
+          repo = "https://argoproj.github.io/argo-helm";
+          version = "8.3.4"; # pick the version you want
+          hash = "sha256:7014017a6c327bd6c682ad71f866f4a0e11508a01e9c39af1b1d8151186cbd61";
+        };
+      };
     };
 
+    #networking.firewall.checkReversePath = false;
+    #networking.firewall.trustedInterfaces = [ "cni0" "flannel.1"];
     networking.firewall.allowedTCPPorts = [ 
-      443 
-      6443
-      10250
+      443  #https
+      6443 #kube-api
+      1883 #mqtt
     ];
+    #networking.firewall.allowedUDPPorts = [
+    #  53
+    #  8472
+    #];
   };
 }
