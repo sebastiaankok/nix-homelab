fbc0a1b4e1445a5818828173a1ba78dbc196917f
Sebastiaan Kok <sebastiaan@linux.com>
2025-09-09 15:57:06 +0200

    chore: add frigate secrets

--- modules/virtual/k3s-home/k3s-secrets.nix
+++ modules/virtual/k3s-home/k3s-secrets.nix
@@ -1,7 +1,11 @@
 { cfg, config, pkgs, pkgs-unstable, ... }:
 {
   sops = {
+    keepGenerations = 0;
     secrets."k3s-home/cloudflare-api.yaml" = {
+      sopsFile = ./secrets.sops.yaml;
+    };
+    secrets."k3s-home/frigate.yaml" = {
       sopsFile = ./secrets.sops.yaml;
     };
   };
