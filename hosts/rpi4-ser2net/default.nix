b40eb69bd269bc7c4f970607072e5a29da2baa53
Sebastiaan Kok <sebastiaan@linux.com>
2025-09-09 15:59:57 +0200

    chore: update ser2net config

--- hosts/rpi4-ser2net/default.nix
+++ hosts/rpi4-ser2net/default.nix
@@ -14,31 +14,33 @@
   environment.etc."ser2net.yaml" = {
     mode = "0755";
     text = ''
-      connection: &con01
-        enable: on
-        accepter: tcp,20108
-        connector: serialdev,/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_da3c386a8212ec11915520c7bd930c07-if00-port0,115200n81,local,dtr=off,rts=off
-        options:
-          kickolduser: true
-      
-      connection: &con02
-        accepter: tcp,20208
-        connector: serialdev,/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AB4HWRSK-if00-port0,115200n81,local
-        options:
-          kickolduser: true
-          banner: *banner
-          telnet-brk-on-sync: true
-      
-      connection: &con03
-        accepter: tcp,20308
-        connector: serialdev,/dev/serial/by-id/usb-RFXCOM_RFXtrx433XL_DO7372CL-if00-port0,38400n81,local
-        options:
-          kickolduser: true
-          telnet-brk-on-sync: true
-      
-      connection: &con04
-        accepter: tcp,20408
-        connector: serialdev,/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0,1200n81,local
+connection: &con01
+  enable: on
+  accepter: tcp,20108
+  connector: serialdev,/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_da3c386a8212ec11915520c7bd930c07-if00-port0,115200n81,local,dtr=off,rts=off
+  options:
+    kickolduser: true
+
+connection: &con02
+  accepter: tcp,20208
+  connector: serialdev,/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AB4HWRSK-if00-port0,115200n81,local
+  options:
+    kickolduser: true
+    banner: *banner
+    telnet-brk-on-sync: true
+
+connection: &con03
+  accepter: tcp,20308
+  connector: serialdev,/dev/serial/by-id/usb-RFXCOM_RFXtrx433XL_DO7372CL-if00-port0,38400n81,local
+  options:
+    kickolduser: true
+    telnet-brk-on-sync: true
+
+connection: &con04
+  accepter: tcp,20408
+  connector: serialdev,/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0,1200n81,local
+  options:
+    kickolduser: true
     '';
   };
 
