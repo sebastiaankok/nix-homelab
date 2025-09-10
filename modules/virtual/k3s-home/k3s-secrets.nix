c546dbe784debda8d2811b9a7d47aa8d74b81508
Sebastiaan Kok <sebastiaan@linux.com>
2025-09-10 23:59:43 +0200

    chore: add secrets

--- modules/virtual/k3s-home/k3s-secrets.nix
+++ modules/virtual/k3s-home/k3s-secrets.nix
@@ -8,5 +8,20 @@
     secrets."k3s-home/frigate.yaml" = {
       sopsFile = ./secrets.sops.yaml;
     };
+    secrets."k3s-home/velero-repo-password.yaml" = {
+      sopsFile = ./secrets.sops.yaml;
+    };
+    secrets."k3s-home/velero-b2-credentials.yaml" = {
+      sopsFile = ./secrets.sops.yaml;
+    };
+    secrets."k3s-home/cnpg-b2-credentials.yaml" = {
+      sopsFile = ./secrets.sops.yaml;
+    };
+    secrets."k3s-home/postgresql-17-postgresql-user-immich.yaml" = {
+      sopsFile = ./secrets.sops.yaml;
+    };
+    secrets."k3s-home/immich-postgresql-user-immich.yaml" = {
+      sopsFile = ./secrets.sops.yaml;
+    };
   };
 }
