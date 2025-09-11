454ffc0e713a3a1a7bbb52a2fe332550a1a12148
Sebastiaan Kok <sebastiaan@linux.com>
2025-09-11 12:58:27 +0200

    chore: update secrets

--- modules/virtual/k3s-home/k3s-secrets.nix
+++ modules/virtual/k3s-home/k3s-secrets.nix
@@ -17,7 +17,7 @@
     secrets."k3s-home/cnpg-b2-credentials.yaml" = {
       sopsFile = ./secrets.sops.yaml;
     };
-    secrets."k3s-home/postgresql-17-postgresql-user-immich.yaml" = {
+    secrets."k3s-home/postgresql-15-immich.yaml" = {
       sopsFile = ./secrets.sops.yaml;
     };
     secrets."k3s-home/immich-postgresql-user-immich.yaml" = {
