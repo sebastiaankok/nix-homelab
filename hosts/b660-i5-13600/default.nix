8c4e4fbeb6a2d68f394c40968d4a1fcba9679a83
Sebastiaan Kok <sebastiaan@linux.com>
2025-09-12 12:16:26 +0200

    chore: clean up

--- hosts/b660-i5-13600/default.nix
+++ hosts/b660-i5-13600/default.nix
@@ -7,33 +7,9 @@
       user = "sebastiaan";
       interface = "enp3s0";
       sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTvwNAE0ZUIgEZRlZqw48o5Sw8gZuCPaYUPUHEp/vtg sebastiaan@linux.com";
-      #system = {
-      #  acme = {
-      #    enable = true;
-      #  };
-      #};
       services = {
-        # photos
-        #immich = {
-        #  enable = true;
-        #};
-        # nvr
-        # frigate.enable = true;
-        # home automation
-        ## Disable migration to k3s
-        # home-assistant.enable = true;
-        # mosquitto.enable = true;
-        # zigbee2mqtt.enable = true;
-        # kamstrup-mqtt.enable = true;
-
         # system
         prometheus.enable = true;
-        # ai
-        # anythingllm.enable = true;
-        # network
-        # wol-proxy.enable = true; # Added wol-proxy service and enabled it.
-        # k3s
-        k3s.enable = false;
       };
     };
   };
